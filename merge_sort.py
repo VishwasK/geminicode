6e02f6b2f71fe7c91883483479bf7afe72abc784
VishwasK <vkarhade@gmail.com>
2024-12-13 15:13:10 -0500

    feat: Add generated code for merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -1,33 +1,28 @@
 def merge_sort(arr):
-    if len(arr) > 1:
-        mid = len(arr) // 2
-        left = arr[:mid]
-        right = arr[mid:]
+    if len(arr) <= 1:
+        return arr
 
-        merge_sort(left)
-        merge_sort(right)
+    mid = len(arr) // 2
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
 
-        i = 0
-        j = 0
-        k = 0
+    return merge(left_half, right_half)
 
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                arr[k] = left[i]
-                i += 1
-            else:
-                arr[k] = right[j]
-                j += 1
-            k += 1
 
-        while i < len(left):
-            arr[k] = left[i]
-            i += 1
-            k += 1
+def merge(left, right):
+    merged = []
+    left_index = 0
+    right_index = 0
 
-        while j < len(right):
-            arr[k] = right[j]
-            j += 1
-            k += 1
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] <= right[right_index]:
+            merged.append(left[left_index])
+            left_index += 1
+        else:
+            merged.append(right[right_index])
+            right_index += 1
 
-    return arr
+    merged.extend(left[left_index:])
+    merged.extend(right[right_index:])
+
+    return merged
